da842145e7b95fbfab3728d12e87a7cb94839530
patan@saro <48280928+patan3saro@users.noreply.github.com>
2025-03-28 12:34:07 +0100

    Update car-data-tag.cc

--- FirstConf/car-data-tag.cc
+++ FirstConf/car-data-tag.cc
@@ -1,3 +1,10 @@
+/**
+ * CarDataTag implementation - A tag class for attaching vehicle data to ns-3 packets
+ * 
+ * This class provides functionality to serialize and deserialize vehicle information
+ * like position, timestamp, node ID, and queue status for transmission between nodes.
+ */
+
 #include "car-data-tag.h"
 #include "ns3/log.h"
 #include "ns3/simulator.h"
@@ -5,135 +12,185 @@
 namespace ns3 {
 
 NS_LOG_COMPONENT_DEFINE("CarDataTag");
-NS_OBJECT_ENSURE_REGISTERED (CarDataTag);
+NS_OBJECT_ENSURE_REGISTERED(CarDataTag);
 
-CarDataTag::CarDataTag() {
-	m_timestamp = Simulator::Now();
-	m_nodeId = -1;
-	m_queue_state = 0;
-}
-CarDataTag::CarDataTag(uint32_t node_id) {
-	m_timestamp = Simulator::Now();
-	m_nodeId = node_id;
-	m_queue_state = 0;
-}
-
-CarDataTag::~CarDataTag() {
-}
-
-//Almost all car tags will have similar implementation of GetTypeId and GetInstanceTypeId
-TypeId CarDataTag::GetTypeId (void)
+CarDataTag::CarDataTag() 
 {
-  static TypeId tid = TypeId ("ns3::CarDataTag")
-    .SetParent<Tag> ()
-    .AddConstructor<CarDataTag> ();
-  return tid;
-}
-TypeId CarDataTag::GetInstanceTypeId (void) const
-{
-  return CarDataTag::GetTypeId ();
+    m_timestamp = Simulator::Now();
+    m_nodeId = -1;
+    m_queue_state = 0;
+    m_queue_max_packets = 0;
 }
 
-/** The size required for the data contained within tag is:
- *   	size needed for a ns3::Vector for position +
- * 		size needed for a ns3::Time for timestamp + 
- * 		size needed for a uint32_t for node id
- */
-uint32_t CarDataTag::GetSerializedSize (void) const
+CarDataTag::CarDataTag(uint32_t nodeId) 
 {
-	return sizeof(Vector) + sizeof (ns3::Time) + 3*sizeof(uint32_t);
+    m_timestamp = Simulator::Now();
+    m_nodeId = nodeId;
+    m_queue_state = 0;
+    m_queue_max_packets = 0;
 }
+
+CarDataTag::~CarDataTag() 
+{
+    // Nothing to clean up
+}
+
+TypeId 
+CarDataTag::GetTypeId(void)
+{
+    static TypeId tid = TypeId("ns3::CarDataTag")
+        .SetParent<Tag>()
+        .AddConstructor<CarDataTag>();
+    return tid;
+}
+
+TypeId 
+CarDataTag::GetInstanceTypeId(void) const
+{
+    return CarDataTag::GetTypeId();
+}
+
 /**
- * The order of how you do Serialize() should match the order of Deserialize()
+ * Calculate serialized size for the tag
+ * 
+ * Size includes:
+ *   - Vector for position (3 doubles)
+ *   - Time for timestamp (1 double)
+ *   - Node ID, queue state, and max queue size (3 uint32_t)
  */
-void CarDataTag::Serialize (TagBuffer i) const
+uint32_t 
+CarDataTag::GetSerializedSize(void) const
 {
-	//we store timestamp first
-	i.WriteDouble(m_timestamp.GetDouble());
-
-	//then we store the position
-	i.WriteDouble (m_currentPosition.x);
-	i.WriteDouble (m_currentPosition.y);
-	i.WriteDouble (m_currentPosition.z);
-
-	//Then we store the node ID
-	i.WriteU32(m_nodeId);
-	i.WriteU32(m_queue_state);
-	i.WriteU32(m_queue_max_packets);
+    return sizeof(Vector) + sizeof(ns3::Time) + 3 * sizeof(uint32_t);
 }
-/** This function reads data from a buffer and store it in class's instance variables.
- */
-void CarDataTag::Deserialize (TagBuffer i)
-{
-	//We extract what we stored first, so we extract the timestamp
-	m_timestamp =  Time::FromDouble (i.ReadDouble(), Time::NS);;
 
-	//Then the position
-	m_currentPosition.x = i.ReadDouble();
-	m_currentPosition.y = i.ReadDouble();
-	m_currentPosition.z = i.ReadDouble();
-	//Finally, we extract the node id
-	m_nodeId = i.ReadU32();
-	m_queue_state = i.ReadU32();
-	m_queue_max_packets = i.ReadU32();
-
-}
 /**
- * This function can be used with ASCII traces if enabled. 
+ * Serialize tag data to buffer
+ * 
+ * The order of serialization must match the order in Deserialize()
  */
-void CarDataTag::Print (std::ostream &os) const
+void 
+CarDataTag::Serialize(TagBuffer i) const
 {
-  os << "Car Data --- Node :" << m_nodeId <<  "\t(" << m_timestamp  << ")" << " Pos (" << m_currentPosition << ")";
+    // Store timestamp
+    i.WriteDouble(m_timestamp.GetDouble());
+    
+    // Store position vector
+    i.WriteDouble(m_currentPosition.x);
+    i.WriteDouble(m_currentPosition.y);
+    i.WriteDouble(m_currentPosition.z);
+    
+    // Store node ID and queue information
+    i.WriteU32(m_nodeId);
+    i.WriteU32(m_queue_state);
+    i.WriteU32(m_queue_max_packets);
 }
 
-//Your accessor and mutator functions 
-uint32_t CarDataTag::GetNodeId() {
-	return m_nodeId;
+/**
+ * Deserialize tag data from buffer
+ * 
+ * Reads data in the same order it was written by Serialize()
+ */
+void 
+CarDataTag::Deserialize(TagBuffer i)
+{
+    // Extract timestamp
+    m_timestamp = Time::FromDouble(i.ReadDouble(), Time::NS);
+    
+    // Extract position
+    m_currentPosition.x = i.ReadDouble();
+    m_currentPosition.y = i.ReadDouble();
+    m_currentPosition.z = i.ReadDouble();
+    
+    // Extract node ID and queue information
+    m_nodeId = i.ReadU32();
+    m_queue_state = i.ReadU32();
+    m_queue_max_packets = i.ReadU32();
 }
 
-void CarDataTag::SetNodeId(uint32_t node_id) {
-	m_nodeId = node_id;
-}
-Vector CarDataTag::GetPosition(void) {
-	return m_currentPosition;
-}
-
-Time CarDataTag::GetTimestamp() {
-	return m_timestamp;
-}
-
-std::string CarDataTag::GetDestinationMacAddress(){
-     return destinationMacAddress;
+/**
+ * Print tag information for ASCII traces
+ */
+void 
+CarDataTag::Print(std::ostream &os) const
+{
+    os << "Car Data --- Node: " << m_nodeId 
+       << "\t(" << m_timestamp << ") "
+       << "Pos (" << m_currentPosition << ")";
 }
 
-uint32_t CarDataTag::GetQueueState(){
-	return m_queue_state;
+// Accessor methods
+uint32_t 
+CarDataTag::GetNodeId() 
+{
+    return m_nodeId;
 }
 
-uint32_t CarDataTag::GetQueueMaxPackets(){
-	return m_queue_max_packets;
+Vector 
+CarDataTag::GetPosition(void) 
+{
+    return m_currentPosition;
 }
 
-void CarDataTag::SetPosition(Vector pos) {
-	m_currentPosition = pos;
+Time 
+CarDataTag::GetTimestamp() 
+{
+    return m_timestamp;
 }
 
-void CarDataTag::SetTimestamp(Time t) {
-	m_timestamp = t;
+std::string 
+CarDataTag::GetDestinationMacAddress()
+{
+    return destinationMacAddress;
 }
 
-void CarDataTag::SetDestinationMacAddress(std::string  destinationMacAddr){
-	destinationMacAddress=destinationMacAddr;
+uint32_t 
+CarDataTag::GetQueueState()
+{
+    return m_queue_state;
 }
 
-void CarDataTag::SetQueueState(uint32_t queue_state){
-	m_queue_state = queue_state;
+uint32_t 
+CarDataTag::GetQueueMaxPackets()
+{
+    return m_queue_max_packets;
 }
 
-void CarDataTag::SetQueueMaxPackets(uint32_t queue_max_packets){
-	m_queue_max_packets = queue_max_packets;
+// Mutator methods
+void 
+CarDataTag::SetNodeId(uint32_t nodeId) 
+{
+    m_nodeId = nodeId;
 }
 
-} /* namespace ns3 */
+void 
+CarDataTag::SetPosition(Vector pos) 
+{
+    m_currentPosition = pos;
+}
 
+void 
+CarDataTag::SetTimestamp(Time t) 
+{
+    m_timestamp = t;
+}
 
+void 
+CarDataTag::SetDestinationMacAddress(std::string destinationMacAddr)
+{
+    destinationMacAddress = destinationMacAddr;
+}
+
+void 
+CarDataTag::SetQueueState(uint32_t queueState)
+{
+    m_queue_state = queueState;
+}
+
+void 
+CarDataTag::SetQueueMaxPackets(uint32_t queueMaxPackets)
+{
+    m_queue_max_packets = queueMaxPackets;
+}
+
+} // namespace ns3
