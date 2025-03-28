0c36cd1d8fbde06621c32e4eee6627c3f7d23b2d
patan@saro <48280928+patan3saro@users.noreply.github.com>
2025-03-28 12:37:45 +0100

    Update cloud-application.h

--- FirstConf/cloud-application.h
+++ FirstConf/cloud-application.h
@@ -1,40 +1,97 @@
+/**
+ * CloudApplication - A cloud server application for task offloading in ns-3
+ *
+ * This class implements a cloud computing application that receives packets from
+ * edge servers, processes them based on cloud computation capacity, and returns
+ * results to the pedestrian nodes.
+ */
+
+#ifndef CLOUD_APPLICATION_H
+#define CLOUD_APPLICATION_H
+
 #include "ns3/socket.h"
 #include "ns3/application.h"
+#include "ns3/ipv4-address.h"
 
-using namespace ns3;
+namespace ns3 {
 
-namespace ns3
+/**
+ * \brief Implementation of a cloud computing application
+ *
+ * This class handles packet reception, processing with simulated computation delay,
+ * and response delivery for cloud-based task offloading.
+ */
+class CloudApplication : public Application 
 {
-  class CloudApplication : public Application 
-  {
-    public:
-      CloudApplication ();
-      virtual ~CloudApplication ();
+public:
+  /**
+   * \brief Get the TypeId
+   *
+   * \return The TypeId for this class
+   */
+  static TypeId GetTypeId();
+  
+  /**
+   * \brief Get the instance TypeId
+   *
+   * \return The TypeId for this instance
+   */
+  virtual TypeId GetInstanceTypeId() const;
+  
+  /**
+   * \brief Default constructor
+   */
+  CloudApplication();
+  
+  /**
+   * \brief Destructor
+   */
+  virtual ~CloudApplication();
+  
+  /**
+   * \brief Handle incoming packets on port 7777
+   *
+   * This method processes packets received at the cloud server,
+   * simulates computation delay, and schedules response packets.
+   *
+   * \param socket The socket that received the packet
+   */
+  void HandleReadOne(Ptr<Socket> socket);
+  
+  /**
+   * \brief Send a packet to the specified destination
+   *
+   * This method sends a processed packet back to the source pedestrian.
+   *
+   * \param packet The packet to send
+   * \param destination The destination IPv4 address
+   * \param port The destination port number
+   */
+  void SendPacket(Ptr<Packet> packet, Ipv4Address destination, uint16_t port);
+  
+private:
+  /**
+   * \brief Set up a receiving socket on the specified port
+   *
+   * \param socket The socket to set up
+   * \param port The port to bind to
+   */
+  void SetupReceiveSocket(Ptr<Socket> socket, uint16_t port);
+  
+  /**
+   * \brief Initialize the application
+   *
+   * Called at the start of the simulation to set up sockets.
+   */
+  virtual void StartApplication();
+  
+  Ptr<Socket> m_recv_socket1;  ///< Socket for receiving packets on port m_port1
+  Ptr<Socket> m_recv_socket2;  ///< Socket for receiving packets on a different port (unused)
+  uint16_t m_port1;            ///< Port for receiving packets (7777)
+  uint16_t m_port_sending;     ///< Port for sending processed packets (9999)
+  Ptr<Socket> m_send_socket;   ///< Socket for sending processed packets
+};
 
-      static TypeId GetTypeId ();
-      virtual TypeId GetInstanceTypeId () const;
+} // namespace ns3
 
-      /** \brief handles incoming packets on port 7777
-       */
-      void HandleReadOne (Ptr<Socket> socket);
-
-      /** \brief Send an outgoing packet. This creates a new socket every time (not the best solution)
-      */
-      void SendPacket (Ptr<Packet> packet, Ipv4Address destination, uint16_t port);
-
-    private:
-      
-      
-      void SetupReceiveSocket (Ptr<Socket> socket, uint16_t port);
-      virtual void StartApplication ();
-
-
-      Ptr<Socket> m_recv_socket1; /**< A socket to receive on a specific port */
-      Ptr<Socket> m_recv_socket2; /**< A socket to receive on a specific port */
-      uint16_t m_port1; 
-      uint16_t m_port_sending;
-
-      Ptr<Socket> m_send_socket; /**< A socket to listen on a specific port */
-  };
-}
-
+#endif // CLOUD_APPLICATION_H
