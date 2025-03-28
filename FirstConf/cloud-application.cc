3712c51b74ed7e8c4c0323620a46d369b3896289
patan@saro <48280928+patan3saro@users.noreply.github.com>
2025-03-28 12:36:33 +0100

    Update cloud-application.cc

--- FirstConf/cloud-application.cc
+++ FirstConf/cloud-application.cc
@@ -1,5 +1,12 @@
+/**
+ * CloudApplication - Implementation of cloud server application for task offloading
+ *
+ * This class implements a cloud computing application that receives packets from
+ * pedestrian nodes via edge servers, processes them based on computation capacity,
+ * and returns results directly to the pedestrian nodes.
+ */
+
 #include "ns3/log.h"
-#include "cloud-application.h"
 #include "ns3/udp-socket.h"
 #include "ns3/simulator.h"
 #include "ns3/csma-net-device.h"
@@ -7,9 +14,12 @@
 #include "ns3/arp-header.h"
 #include "ns3/ipv4-header.h"
 #include "ns3/udp-header.h"
+
+#include "cloud-application.h"
 #include "globals.h"
 #include "packet-data-tag.h"
 
+// Terminal color codes for logging
 #define PURPLE_CODE "\033[95m"
 #define CYAN_CODE "\033[96m"
 #define TEAL_CODE "\033[36m"
@@ -21,101 +31,115 @@
 #define BOLD_CODE "\033[1m"
 #define END_CODE "\033[0m"
 
+namespace ns3 {
 
+NS_LOG_COMPONENT_DEFINE("CloudApplication");
+NS_OBJECT_ENSURE_REGISTERED(CloudApplication);
 
-namespace ns3
+TypeId
+CloudApplication::GetTypeId()
 {
-  NS_LOG_COMPONENT_DEFINE("CloudApplication");
-  NS_OBJECT_ENSURE_REGISTERED(CloudApplication);
+  static TypeId tid = TypeId("ns3::CloudApplication")
+                          .AddConstructor<CloudApplication>()
+                          .SetParent<Application>();
+  return tid;
+}
 
-  TypeId
-  CloudApplication::GetTypeId()
+TypeId
+CloudApplication::GetInstanceTypeId() const
+{
+  return CloudApplication::GetTypeId();
+}
+
+CloudApplication::CloudApplication()
+  : m_port1(7777),
+    m_port_sending(9999)
+{
+  // Nothing else to initialize
+}
+
+CloudApplication::~CloudApplication()
+{
+  // Nothing to clean up
+}
+
+void 
+CloudApplication::SetupReceiveSocket(Ptr<Socket> socket, uint16_t port)
+{
+  InetSocketAddress local = InetSocketAddress(Ipv4Address::GetAny(), port);
+  if (socket->Bind(local) == -1)
   {
-    static TypeId tid = TypeId("ns3::CloudApplication")
-                            .AddConstructor<CloudApplication>()
-                            .SetParent<Application>();
-    return tid;
+    NS_FATAL_ERROR("Failed to bind socket");
   }
+}
 
-  TypeId
-  CloudApplication::GetInstanceTypeId() const
-  {
-    return CloudApplication::GetTypeId();
-  }
+void 
+CloudApplication::StartApplication()
+{
+  // Create and set up receiving socket
+  TypeId tid = TypeId::LookupByName("ns3::UdpSocketFactory");
+  m_recv_socket1 = Socket::CreateSocket(GetNode(), tid);
+  SetupReceiveSocket(m_recv_socket1, m_port1);
+  m_recv_socket1->SetRecvCallback(MakeCallback(&CloudApplication::HandleReadOne, this));
+  
+  // Create sending socket
+  m_send_socket = Socket::CreateSocket(GetNode(), tid);
+}
 
-  CloudApplication::CloudApplication()
+void 
+CloudApplication::HandleReadOne(Ptr<Socket> socket)
+{
+  NS_LOG_FUNCTION(this << socket);
+  Ptr<Packet> packet;
+  Address from;
+  Address localAddress;
+  
+  while ((packet = socket->RecvFrom(from)))
   {
-    m_port1 = 7777;
-    m_port_sending = 9999;
-  }
-  CloudApplication::~CloudApplication()
-  {
-  }
-  void CloudApplication::SetupReceiveSocket(Ptr<Socket> socket, uint16_t port)
-  {
-    InetSocketAddress local = InetSocketAddress(Ipv4Address::GetAny(), port);
-    if (socket->Bind(local) == -1)
-    {
-      NS_FATAL_ERROR("Failed to bind socket");
-    }
-  }
-  void CloudApplication::StartApplication()
-  {
-    //Receive sockets
-    TypeId tid = TypeId::LookupByName("ns3::UdpSocketFactory");
-    m_recv_socket1 = Socket::CreateSocket(GetNode(), tid);
-
-    SetupReceiveSocket(m_recv_socket1, m_port1);
-
-    m_recv_socket1->SetRecvCallback(MakeCallback(&CloudApplication::HandleReadOne, this));
-
-    //Send Socket
-    m_send_socket = Socket::CreateSocket(GetNode(), tid);
-  }
-
-  void CloudApplication::HandleReadOne(Ptr<Socket> socket)
-  {
-    NS_LOG_FUNCTION(this << socket);
-    Ptr<Packet> packet;
-    Address from;
-    Address localAddress;
+    NS_LOG_INFO(TEAL_CODE << "HandleReadOne: Cloud received a Packet of size: " 
+                << packet->GetSize() << " at time " 
+                << Now().GetSeconds() << END_CODE);
+    NS_LOG_INFO(packet->ToString());
     
-    while ((packet = socket->RecvFrom(from)))
+    // Extract destination address from packet tag
+    Ipv4Address pedestrianAddress;
+    
+    // Process packet metadata
+    PacketDataTag tag;
+    if (packet->RemovePacketTag(tag))
     {
-      NS_LOG_INFO(TEAL_CODE << "HandleReadOne : Cloud received a Packet of size: " << packet->GetSize() << " at time " << Now().GetSeconds() << END_CODE);
-      NS_LOG_INFO(packet->ToString());
+      // Update tag with processing information
+      tag.SetUplinkTime(Seconds(Now().GetSeconds()));
+      tag.SetElaborationTime(Seconds(tag.GetWorkload() / cloud_computation_capacity));
+      tag.SetQueueingTime(Seconds(Now().GetSeconds()));
       
-      Ipv4Address pedestrianAddress;
+      // Get pedestrian address for return packet
+      pedestrianAddress = tag.GetSourcePedestrianIPv4Address();
       
-      	//setting packet infos in the tag
-	    PacketDataTag tag;
-          
-	    if (packet->RemovePacketTag (tag))
-	    {
-	    	tag.SetUplinkTime(Seconds(Now().GetSeconds()));
-	    	tag.SetElaborationTime(Seconds(tag.GetWorkload()/cloud_computation_capacity));
-	    	tag.SetQueueingTime(Seconds(Now().GetSeconds()));
-	    	
-	    	pedestrianAddress = tag.GetSourcePedestrianIPv4Address();
-	    	
-		packet->AddPacketTag (tag);
-	    }
-   	
-		
-
-        //create new packet
-	Ptr <Packet> packet_tmp = packet->Copy();
-   
-      Simulator::Schedule (Seconds (packet->GetSize()/cloud_computation_capacity), &CloudApplication::SendPacket, this, packet_tmp, pedestrianAddress, m_port_sending);
+      // Reattach the updated tag
+      packet->AddPacketTag(tag);
     }
     
+    // Create a copy of the packet for sending
+    Ptr<Packet> packetCopy = packet->Copy();
+    
+    // Schedule packet transmission after processing delay
+    double processingDelay = packet->GetSize() / cloud_computation_capacity;
+    Simulator::Schedule(Seconds(processingDelay), 
+                       &CloudApplication::SendPacket, 
+                       this, 
+                       packetCopy, 
+                       pedestrianAddress, 
+                       m_port_sending);
   }
+}
 
-  void CloudApplication::SendPacket(Ptr<Packet> packet, Ipv4Address destination, uint16_t port)
-  {
-    NS_LOG_FUNCTION (this << packet << destination << port);
-    m_send_socket->Connect(InetSocketAddress(Ipv4Address::ConvertFrom(destination), port));
-    m_send_socket->Send(packet);
-  }
+void 
+CloudApplication::SendPacket(Ptr<Packet> packet, Ipv4Address destination, uint16_t port)
+{
+  NS_LOG_FUNCTION(this << packet << destination << port);
+  m_send_socket->Connect(InetSocketAddress(Ipv4Address::ConvertFrom(destination), port));
+  m_send_socket->Send(packet);
+}
 
 } // namespace ns3
